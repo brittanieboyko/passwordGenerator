157078e6642871dd375c0b386cbefd6d0c010a57
brittanieb <brittanie.boyko@gmail.com>
2019-12-02 14:51:17 -0500

    updated createPassword() to generate a random password of user's desired length using all possible characters

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 
-function askUserForLength() {
+function createPassword() {
     var passwordLength = prompt("How long do you want your password to be?", "8");
-
-    if (passwordLength >= 8 && passwordLength <= 128) {
-        console.log(passwordLength);
-    }
-
     var specialChars = confirm("Do you want to include special characters in your password?");
     var lowercaseChars = confirm("Do you want to include lowercase characters in your password?");
     var uppercaseChars = confirm("Do you want to include uppercase characters in your password?");
-}
+
+    if (passwordLength >= 8 && passwordLength <= 128) {
+        var password = " ";
+        var allCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
+        for (var i = 0; i < passwordLength; i++) {
+           password = password + allCharacters.charAt(Math.floor(Math.random() * passwordLength));
+        }
+
+        console.log(password);
+    };
+};
+ 
